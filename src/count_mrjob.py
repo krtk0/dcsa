5f1a40e0eb330c7a945a509d2d104e863d8a0e68
ydukhno <edukhno@gmail.com>
2018-01-02 14:27:15 +0200

    Get the output of top-10 frequently used words except urls

--- src/count_mrjob.py
+++ src/count_mrjob.py
@@ -1,10 +1,18 @@
-from mrjob.job import MRJob
+from mrjob.job import MRJob, MRStep
 import re
+
 
 WORD_RE = re.compile(r"[\w']+")
 
 
 class CountMRJob(MRJob):
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   reducer=self.reducer),
+            MRStep(reducer=self.final)
+        ]
 
     def mapper(self, _, line):
         for word in WORD_RE.findall(line):
@@ -14,7 +22,16 @@ class CountMRJob(MRJob):
         yield (word, sum(counts))
 
     def reducer(self, word, counts):
-        yield (word, sum(counts))
+        yield None, (word, sum(counts))
+
+    def final(self, word, counts):
+        self._result = []
+        for v in counts:
+            self._result.append(v)
+        self._result.sort(key=lambda x: x[1])
+        count = len(self._result)
+        for m in range(count - 10, count):
+            yield self._result[m]
 
 
 if __name__ == '__main__':
